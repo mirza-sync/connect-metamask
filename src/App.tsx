37745f4c61c775159848dcf2e93c8100e8016e4c
mirza-sync <mirzasyahmi123@gmail.com>
2022-12-14 12:28:29 +0800

    Add feature: Connect metamask

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isConnected, setIsConnected] = useState(false);
+
+  const connect = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      try {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        setIsConnected(true);
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      alert("You don't have Metamask installed");
+    }
+  };
 
   return (
     <div className="App">
-      <h2>You are not connected to Metamask</h2>
-      <div className="card">
-        <button className='btn' onClick={() => null}>
-          <img src='/metamask-icon.svg' />
-          <span>Connect</span>
-        </button>
-      </div>
+      {!isConnected ? (
+        <>
+          <h2>You are not connected to Metamask</h2>
+          <div className="card">
+            <button className="btn" onClick={() => connect()}>
+              <img src="/metamask-icon.svg" />
+              <span>Connect</span>
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <h2>Connected: 0xabc....1ve34e</h2>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
