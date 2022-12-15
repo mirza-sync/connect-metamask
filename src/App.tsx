8c8971fdc2d550462dcab568dae50a244424dcb3
mirza-sync <mirzasyahmi123@gmail.com>
2022-12-15 23:53:23 +0800

    Add feature: Show connected network

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { MetaMaskInpageProvider } from "@metamask/providers";
+import { Network, networkList } from "../constants/networkList";
 
 declare global {
   interface Window {
@@ -9,13 +10,40 @@ declare global {
 }
 
 function App() {
-  const [isConnected, setIsConnected] = useState(false);
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+  const [network, setNetwork] = useState<Network | undefined>(undefined);
 
   useEffect(() => {
     if (window.ethereum.selectedAddress != null) {
       setIsConnected(true);
+      getNetworkDetails(window.ethereum.chainId);
     }
   }, []);
+
+  useEffect(() => {
+    window.ethereum.on("chainChanged", getNetworkDetails);
+
+    return () => {
+      (window as any).ethereum.removeListener(
+        "chainChanged",
+        getNetworkDetails
+      );
+    };
+  }, []);
+
+  const getNetworkDetails = async (chainId: any) => {
+    const chainNum = chainIdToNum(chainId);
+    const network = networkList.filter(
+      (network) => chainNum === network.chainId
+    )[0];
+    setNetwork(network);
+  };
+
+  const chainIdToNum = (chainId: string) => {
+    const hex = chainId.split("0x")[1];
+    const chainNum = parseInt(hex, 16);
+    return chainNum;
+  };
 
   const connect = async () => {
     if (typeof window.ethereum !== "undefined") {
@@ -41,9 +69,13 @@ function App() {
           </button>
         </div>
       ) : (
-        <>
+        <div>
           <h2>Connected: {window.ethereum.selectedAddress}</h2>
-        </>
+          <div>
+            <div>Network name: {network?.name ?? "Unknown"}</div>
+            <div>Chain ID: {network?.chainId}</div>
+          </div>
+        </div>
       )}
     </div>
   );
